732ff2be26b8b17e050a257c4f863f5ce5bc902a
Fred Wenzel <fwenzel@mozilla.com>
2012-05-03 11:09:53 -0700

    age and lomo filters

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 (function() {
   var filters = {
-    filter1: function() {
+    age: function() {
       this
         .saturation(20)
         .gamma(1.4)
@@ -10,6 +10,18 @@
         .contrast(5)
         .exposure(15)
         .vignette(300, 60)
+        .render()
+    },
+
+    lomo: function() {
+      this
+        .brightness(15)
+        .exposure(15)
+        .curves('rgb', [0, 0], [200, 0], [155, 255], [255, 255])
+        .saturation(-20)
+        .gamma(1.8)
+        .vignette("50%", 60)
+        .brightness(5)
         .render()
     }
   };
