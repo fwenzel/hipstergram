0692a69ac43eeabe404429e56907c5c142d0dce5
Fred Wenzel <fwenzel@mozilla.com>
2012-05-03 10:54:18 -0700

    Take picture, apply filter according to button pressed.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,22 @@
 "use strict";
 
-document.getElementById('filter').addEventListener('click', function(event) {
-  Caman("example.jpg", "canvas", function () {
-    this
-      .saturation(20)
-      .gamma(1.4)
-      .vintage()
-      .contrast(5)
-      .exposure(15)
-      .vignette(300, 60)
-      .render()
-  })
-})
+(function() {
+  var filters = {
+    filter1: function() {
+      this
+        .saturation(20)
+        .gamma(1.4)
+        .vintage()
+        .contrast(5)
+        .exposure(15)
+        .vignette(300, 60)
+        .render()
+    }
+  };
+
+  $('#filters button').click(function(e) {
+    e.preventDefault();
+    Caman("example.jpg", "canvas", filters[$(this).data('filter')]);
+  });
+
+})()
